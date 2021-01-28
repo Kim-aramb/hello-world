19a2d29cb0f85c8adb82c2e8ccc95a5ef66483c2
Kim-aramb <78109025+Kim-aramb@users.noreply.github.com>
2021-01-27 19:19:12 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/******************************
+* Java Program Lab 1
+******************************
+* Kimberly Arambula 
+* 1/ 27 / 2020
+* CMSC 255 003 
+**********************************************/
+
+public class Hello {
+public static void main(String[] args) {
+System.out.println("<Kimberly>");
+System.out.println("<Lab 1>");
+System.out.println("<Type out our name as part of the first lab and get familiar with the java programming system and download the necessary programs>");
+System.out.println("<1/27/2020>");
+System.out.println("<CMSC 255 Section 003>");
+System.out.println();
+}
+}
